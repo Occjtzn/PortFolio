2ae75a1bd4665c3e1699b91a10c4430777d866d5
Occcjtzn <thibault.courrieu@outlook.fr>
2025-01-22 13:15:59 +0100

    LastPush 22012025

--- src/Components/HomeSections/HomeSections.jsx
+++ src/Components/HomeSections/HomeSections.jsx
@@ -6,16 +6,16 @@ import { Skills } from './Skills/Skills';
 export const HomeSections = () => {
   return (
     <>
-      <section id="About" className="px-4 sm:px-0">
+      <section id="About" className="px-4 lg:px-0">
         <About />
       </section>
-      <section id="Projects" className="px-4 sm:px-0">
+      <section id="Projects" className="px-4 lg:px-0">
         <Projects />
       </section>
-      <section id="Skills" className="px-4 sm:px-0">
+      <section id="Skills" className="px-4 lg:px-0">
         <Skills />
       </section>
-      <section id="Contact" className="px-4 sm:px-0">
+      <section id="Contact" className="px-4 lg:px-0">
         <Contact />
       </section>
     </>
